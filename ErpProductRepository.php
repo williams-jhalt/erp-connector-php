481155e3813f0b66c93a27e87b4c5694bfa70fad
johnh <johnh@williams-trading.com>
2016-11-17 15:03:49 -0500

    added findAll method

--- ErpProductRepository.php
+++ ErpProductRepository.php
@@ -4,6 +4,36 @@ namespace Williams\Erp;
 
 class ErpProductRepository extends AbstractErpRepository {
 
+    public function findAll($offset = 0, $limit = 1000) {
+
+        $query = "FOR EACH item NO-LOCK "
+                . "WHERE item.company_it = '" . $this->erp->getCompany() . "', "
+                . "EACH wa_item NO-LOCK "
+                . "WHERE wa_item.company_it = item.company_it "
+                . "AND wa_item.item = item.item";
+
+        $response = $this->erp->read($query, "item.item,item.descr,wa_item.ship_location,wa_item.list_price,wa_item.qty_cmtd,wa_item.qty_oh");
+
+        if (empty($response)) {
+            return $null;
+        }
+
+        $result = array();
+        
+        foreach ($response as $item) {
+            $product = new ErpProduct();
+            $product->setItemNumber($item->item_item);
+            $product->setName(join(" ", $item->item_descr));
+            $product->setBinLocation($item->wa_item_ship_location);
+            $product->setPrice($item->wa_item_list_price);
+            $product->setOnHandQuantity($item->wa_item_qty_oh);
+            $product->setCommittedQuantity($item->wa_item_qty_cmtd);
+            $result[] = $product;
+        }
+
+        return $result;
+    }
+
     public function getByItemNumber($itemNumber) {
 
         $query = "FOR EACH item NO-LOCK "
@@ -88,7 +118,7 @@ class ErpProductRepository extends AbstractErpRepository {
             $product->setPrice($item->wa_item_list_price);
             $product->setOnHandQuantity($item->wa_item_qty_oh);
             $product->setCommittedQuantity($item->wa_item_qty_cmtd);
-            
+
             $result[] = $product;
         }
 
